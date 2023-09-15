501b278ea35b83fe04414aaaf10301cca520a2fd
waldymarxthf <iliya.nov223@gmail.com>
2023-09-15 17:04:34 +0300

    [feat] feat: add env

--- waldymarxthf/frontend/src/app/providers/pb-provider.tsx
+++ waldymarxthf/frontend/src/app/providers/pb-provider.tsx
@@ -4,7 +4,7 @@ import { createContext } from "react";
 
 export function PocketBaseProvider({ children }: { children: ReactNode }) {
   // Make sure to register http://127.0.0.1:8090/api/oauth2-redirect as redirect url.
-  const pb = useMemo(() => new PocketBase("http://127.0.0.1:8090"), []);
+  const pb = useMemo(() => new PocketBase(import.meta.env.VITE_PB_URL), []);
   pb.autoCancellation(false);
   const [, setToken] = useState(pb.authStore.token);
   const [user, setUser] = useState(pb.authStore.model);
