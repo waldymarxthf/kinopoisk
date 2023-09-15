a05945f767c9af82c1f1edefaa7287af88701fac
waldymarxthf <iliya.nov223@gmail.com>
2023-09-15 23:51:59 +0300

    [feat] feat: update go server

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "~app/routes/route";
+import { StrictMode } from "react";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
