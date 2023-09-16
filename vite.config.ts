f454a90a760cdb0a0f254c6adac92b89f71d8915
waldymarxthf <iliya.nov223@gmail.com>
2023-09-16 16:49:42 +0300

    [feat] feat: add vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import path, { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
